47a422904f87a95ed8e3b0d32ba4adf10d929766
Jun Omae <jun66j5@gmail.com>
2025-04-21 01:46:16 +0900

    Fix wrong layout of subtickets with Trac 1.4 or later

--- tracsubtickets/htdocs/js/subtickets.js
+++ tracsubtickets/htdocs/js/subtickets.js
@@ -1,11 +1,10 @@
-jQuery(function(){
-  const anchor = document.getElementById('ticket');
-  if (!anchor) {
-    return;
+jQuery(function($) {
+  var ticketbox = document.getElementById('ticketbox');
+  if (ticketbox === null)
+    ticketbox = document.getElementById('ticket');
+  if (ticketbox !== null) {
+    var div = $(document.createElement('div'));
+    div.html(subtickets_div);
+    $(ticketbox).append(div.contents());
   }
-
-  const div = document.createElement('div');
-  div.innerHTML = subtickets_div;
-
-  anchor.appendChild(div);
 });
